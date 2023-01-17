8ff32ad186eed46a3166b93a2b6de3d08f2be8a7
nvii-rweise <r.weise@nvii-media.com>
2023-01-17 11:10:49 +0100

    some configuration things

--- src/Controller/TravelController.php
+++ src/Controller/TravelController.php
@@ -2,40 +2,33 @@
 
 namespace App\Controller;
 
+use Psr\Cache\CacheItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\CacheItem;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class TravelController extends AbstractController
 {
-    private $days = [
-        ["id" => "1", "title" => "Let's go!", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
-        ["id" => "2", "title" => "Wer braucht schon Fahrbahnmarkierung", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
-        ["id" => "3", "title" => "Motorkontrollleuchte", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
-        ["id" => "4", "title" => "Haben wir Diesel getankt?", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
-        ["id" => "5", "title" => "Eishöhlen", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
-        ["id" => "6", "title" => "Italien wir kommen!", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
-        ["id" => "7", "title" => "Vendig!", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
-        ["id" => "8", "title" => "Strand", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
-        ["id" => "9", "title" => "Sonne wir kommen!", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
-        ["id" => "10", "title" => "Slowenien", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."]
-    ];
-
     #[Route("/", name: "app_route_index")]
-    public function index(): Response
+    public function index(CacheInterface $cache): Response
     {
+        dump($cache);
+        $days = $this->getDays($cache);
         return $this->render("travelblog/index.html.twig", [
             "title" => "Reiseblog",
-            "days" => $this->days
+            "days" => $days
         ]);
     }
 
     #[Route("/day/{dayNumber}", name: "app_route_day")]
-    public function day(int $dayNumber): Response
+    public function day(CacheInterface $cache, int $dayNumber): Response
     {
-        $nextDay = count($this->days) === $dayNumber ? 0 : $dayNumber + 1;
+        $days = $this->getDays($cache);
+        $nextDay = count($days) === $dayNumber ? 0 : $dayNumber + 1;
         $prevDay = $dayNumber === 1 ? 0 : $dayNumber - 1;
-        $day = $this->days[$dayNumber - 1];
+        $day = $days[$dayNumber - 1];
         if ($dayNumber) {
             return $this->render("travelblog/day.html.twig", [
                 "title" => "Tag " . $dayNumber,
@@ -44,5 +37,27 @@ class TravelController extends AbstractController
                 "prevDayNumber" => $prevDay
             ]);
         }
+    }
+
+    private function getDays(CacheInterface $cache): array
+    {
+        $days = $cache->get('all_days', function (CacheItemInterface $cacheItem) {
+            $cacheItem->expiresAfter(30);
+            return [
+                ["id" => "1", "title" => "Let's go!", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
+                ["id" => "2", "title" => "Wer braucht schon Fahrbahnmarkierung", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
+                ["id" => "3", "title" => "Motorkontrollleuchte", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
+                ["id" => "4", "title" => "Haben wir Diesel getankt?", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
+                ["id" => "5", "title" => "Eishöhlen", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
+                ["id" => "6", "title" => "Italien wir kommen!", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
+                ["id" => "7", "title" => "Vendig!", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
+                ["id" => "8", "title" => "Strand", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
+                ["id" => "9", "title" => "Sonne wir kommen!", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
+                ["id" => "10", "title" => "Slowenien", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."],
+                ["id" => "11", "title" => "Lecker Essen!", "text" => "Eu consequat voluptate laborum nostrud consequat in cillum nisi sit est in magna sit. Irure adipisicing eiusmod incididunt adipisicing sint amet Lorem magna cupidatat incididunt proident dolore. Ea voluptate Lorem nisi aliqua enim sint adipisicing do aliqua reprehenderit id id magna."]
+            ];
+        });
+
+        return $days;
     }
 }
